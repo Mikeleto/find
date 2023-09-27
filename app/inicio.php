9d070f8e7526f271cdba47180dfb458f8451441e
mikeleto <mikeletorsayayin@gmail.com>
2023-09-27 20:14:40 +0200

    mi primer commit

---
+++ app/inicio.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * La clase aplication maneja la url y lanza los procesos
+ */
+
+ class Application{
+    function __construct(){
+        print "Bienvenido a mi tienda virtual";
+    }
+ }
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once "../app/inicio.php";
+
+$app = new Application();
+?>
