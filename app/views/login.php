10e960e5e81b700f4143c8668db2a326cebd30c7
mikeleto <mikeletorsayayin@gmail.com>
2023-10-02 16:24:05 +0200

    tonteria #3

--- app/views/login.php
+++ app/views/login.php
@@ -1,4 +1,3 @@
-
 <?php include_once 'header.php' ?>
 <div class="card p-4 bg-light">
     <div class="card-header">
@@ -17,16 +16,25 @@
             <div class="form-group text-left mb-2">
                 <input type="checkbox" name="remember">
                 <label for="remember">Recordar</label>
+                
+                </div>
+                <div class="form-group text-left">
+                <input type="submit" value="Enviar" class="btn btn-success">
+            </div>
+        </form>
+    </div>
+    <div class="card-footer">
+        <div class="row">
+            <div class="col-sm-6">
+                <a href="login/alta" class="btn btn-info">Nuevo usuario</a>
+
+                </div>
+
                 <div class="col-sm-6">
                 <a href="login/olvido" class="btn btn-info">¿No recuerdas la contraseña?</a>
-                </div>
-                </div>
+            </div>
         </div>
     </div>
 
-
-                </div>
+    </div>
 <?php include_once 'footer.php' ?>
-           
-</body>
-</html>
