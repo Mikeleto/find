f9b10e6335b055c7e826120dea42ecd917904366
mikeleto <mikeletorsayayin@gmail.com>
2023-10-05 14:23:36 +0200

    hola

--- app/models/login.php
+++ app/models/login.php
@@ -74,4 +74,18 @@ public function existsEmail($email)
 
         return mail($email, $subject, $msg, $headers);
     }
+
+    public function changePassword($id, $password)
+    {
+        $pass = hash_hmac('sha512', $password, 'elperrodesanroque');
+        $sql = 'UPDATE users SET password=:password WHERE id=:id';
+        $params = [
+            ':id' => $id,
+            ':password' => $pass,
+        ];
+        $query = $this->db->prepare($sql);
+        $response = $query->execute($params);
+
+        return $response;
+    }
 }
