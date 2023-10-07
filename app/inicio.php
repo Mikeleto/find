45336b42ba8fc03fc6c9b0e6c1217258d84319bb
mikeleto <mikeletorsayayin@gmail.com>
2023-10-07 19:07:29 +0200

    AdminUser y crear usuarios admin

--- app/inicio.php
+++ app/inicio.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 define('ROOT', DIRECTORY_SEPARATOR);
 define('APP', ROOT . 'app' . ROOT);
 define('ENCRIPTKEY', 'elperrodesanroque');
+define('URL', '/var/www/tiendamvc');
+define('VIEWS', URL . APP . 'views' . ROOT);
 
 require_once "libs/MySQLdb.php";
 require_once "libs/Application.php";
